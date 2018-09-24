72a51cc8202d254c38c991e8e4150524200107a8
thahirakram <thahirakram289@gmail.com>
2018-09-24 10:55:09 +0530

    fcommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-
+import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
+import { ListComponent } from './list/list.component';
+const rooter: Routes = [
+  {
+    path: 'create',
+    component: CreateComponent
+  },
+  {
+    path: 'edit',
+    component: EditComponent
+  },
+  {
+    path: 'list',
+    component: ListComponent
+  }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateComponent,
+    EditComponent,
+    ListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(rooter)
   ],
   providers: [],
   bootstrap: [AppComponent]
